238a178c62e774aff9b43d0f74910a1103381f1a
web Development <shubhangibhapkar35@gmail.com>
2024-10-12 15:44:47 +0530

    express-session

--- Backend/classroom/server.js
+++ Backend/classroom/server.js
@@ -1,19 +1,26 @@
 const express=require("express");
 const app=express();
-const cookieParser=require("cookie-parser");
+// const cookieParser=require("cookie-parser");
+const session=require("express-session");
 
-app.use(cookieParser());
+app.use(session({secret:"mysupersecretecode"}));
 
-app.get("/cookies",(req,res)=>{
-    res.cookie("greet","namaste");
-    res.cookie("madeIn","India");
-    res.send("sent you cookie");
-})
-
-app.get("/",(req,res)=>{
-    console.dir(req.cookies);
-    res.send("Hi i am root!");
+app.get("/test",(req,res)=>{
+    res.send("test successfull!");
 });
+
+// app.use(cookieParser());
+
+// app.get("/cookies",(req,res)=>{
+//     res.cookie("greet","namaste");
+//     res.cookie("madeIn","India");
+//     res.send("sent you cookie");
+// })
+
+// app.get("/",(req,res)=>{
+//     console.dir(req.cookies);
+//     res.send("Hi i am root!");
+// });
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");

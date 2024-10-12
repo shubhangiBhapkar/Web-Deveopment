43571d589e94b98073d424d1325b3dec5eb1885a
web Development <shubhangibhapkar35@gmail.com>
2024-10-12 19:45:44 +0530

    express-session

--- Backend/classroom/server.js
+++ Backend/classroom/server.js
@@ -3,11 +3,24 @@ const app=express();
 // const cookieParser=require("cookie-parser");
 const session=require("express-session");
 
-app.use(session({secret:"mysupersecretecode"}));
+app.use(session({
+    secret:"mysupersecretecode",
+    resave: false,
+    saveUninitialized: true
+}));
 
-app.get("/test",(req,res)=>{
-    res.send("test successfull!");
+app.get("/reqcount",(req,res)=>{
+    if(req.session.count){
+        req.session.count++;
+    }else{
+        req.session.count=1;
+    }
+    res.send(`you send request ${req.session.count} times`);
 });
+
+// app.get("/test",(req,res)=>{
+//     res.send("test successfull!");
+// });
 
 // app.use(cookieParser());
 

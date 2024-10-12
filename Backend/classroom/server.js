fd4ff3ded7c03db06363b8d5e8de426edbda99cc
web Development <shubhangibhapkar35@gmail.com>
2024-10-12 23:31:07 +0530

    flash msg

--- Backend/classroom/server.js
+++ Backend/classroom/server.js
@@ -2,21 +2,43 @@ const express=require("express");
 const app=express();
 // const cookieParser=require("cookie-parser");
 const session=require("express-session");
+const path=require("path");
+const flash=require("connect-flash");
 
-app.use(session({
+app.use(flash());
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+const sessionOptions={
     secret:"mysupersecretecode",
     resave: false,
     saveUninitialized: true
-}));
+};
 
-app.get("/reqcount",(req,res)=>{
-    if(req.session.count){
-        req.session.count++;
-    }else{
-        req.session.count=1;
-    }
-    res.send(`you send request ${req.session.count} times`);
+app.use(session(sessionOptions));
+
+app.get("/register",(req,res)=>{
+    let {name="anonymous"}=req.query;
+    req.session.name=name;
+    console.log(name);
+    res.redirect("/hello");
+    // res.send(req.session.name);
 });
+
+app.get("/hello",(req,res)=>{
+    // res.send(`Hello,${req.session.name}`);
+    res.render("page.ejs",{name:req.session.name,msg:req.flash("success")});
+});
+
+
+// app.get("/reqcount",(req,res)=>{
+//     if(req.session.count){
+//         req.session.count++;
+//     }else{
+//         req.session.count=1;
+//     }
+    // res.send("you send request {req.session.count} times");
+// });
 
 // app.get("/test",(req,res)=>{
 //     res.send("test successfull!");

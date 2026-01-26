49216a7b4ec0640af7569a7deb283d00a31c5862
web Development <shubhangibhapkar35@gmail.com>
2026-01-26 09:28:04 +0530

    Popcorn bill

---
+++ Frontend/JavaScript/Popcorn.js
@@ -0,0 +1,13 @@
+
+    let size ='XL'
+    if(size === 'XL'){
+        console.log("you selected XL size :","Total price is ","250")
+    }else if(size === 'L'){
+        console.log("you selected L size :","Total price is ",200)
+    }else if(size === 'M'){
+        console.log("you selected M size :","Total price is ",150)
+    }else if(size === 'S'){
+        console.log("you selected S size :","Total price is ",100)
+    }else{
+        console.log("Please select a valid size")
+    }

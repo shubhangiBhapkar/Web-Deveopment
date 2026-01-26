09d93a25347199a584c9200e59a23ffe58b1e024
web Development <shubhangibhapkar35@gmail.com>
2026-01-26 09:59:33 +0530

    Nested if-else

---
+++ Frontend/JavaScript/Nested-if-else.js
@@ -0,0 +1,11 @@
+let marks =90
+if(marks >=33){
+    console.log("You passed the exam.")
+    if(marks >= 85){
+        console.log("Grade O")
+    }else{
+        console.log("Grade A")
+    }
+}else{
+    console.log("Better Luck next Time..")
+}
